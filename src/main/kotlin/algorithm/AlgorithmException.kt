ce810536b0154a4ecee916774cc4338588b1fa83
efremova_anna <efremova.anna.vyacheslavovna@gmail.com>
2024-07-01 18:32:17 +0300

    coloring the text, add an exception

--- src/main/kotlin/algorithm/AlgorithmException.kt
+++ src/main/kotlin/algorithm/AlgorithmException.kt
@@ -5,3 +5,5 @@ open class AlgorithmException(text: String) : Exception(text)
 class TooManyVertices(message: String) : AlgorithmException("Message: $message")
 
 class TooManyComponents(message: String): AlgorithmException("Message: $message")
+
+class IsEmptyGraph(message: String): AlgorithmException("Message: $message")
